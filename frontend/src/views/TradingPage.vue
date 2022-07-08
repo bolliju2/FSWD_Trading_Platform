06d8aac5dd4c30bf5af72d30b3bac2468073729e
Besfort Ramadani <71851042+BesRam@users.noreply.github.com>
2022-07-08 02:55:08 +0200

    design tradingPage or buy and sell page

--- frontend/src/views/TradingPage.vue
+++ frontend/src/views/TradingPage.vue
@@ -1,49 +1,67 @@
 <template>
   <ion-page>
-    <ion-card>
-      <ion-card-header>
-        <ion-card-title>Buy and sell coins</ion-card-title>
-      </ion-card-header>      
-      <ion-card-content>
-        <ion-list>
-          <ion-item>
-            <ion-select
-              placeholder="Select currency" @ionChange="selectedCurrency = $event.detail.value; setCurrency(); selectedCurrencyyy = $event.detail.value">
-              <ion-select-option
-                :value="symbol"
-                :key="symbol"
-                v-for="symbol in currencyNames"
-                >{{ symbol }}</ion-select-option
-              >
-            </ion-select>
-          </ion-item>
-          <ion-item>
-            <p v-if="currency">
-              Current Value: <strong> {{ currency.historicalValues[currency.historicalValues.length - 1].value }} </strong>
-            </p>
-          </ion-item>
-          <ion-item>
-            <ion-input
-              type="number"
-              placeholder="Enter the amount of coins" 
-              @ionChange="amntCoinss = $event.detail.value" v-model="amntCoins"
-              >
-            </ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-button
-              @click="calculateValue(currency.historicalValues[currency.historicalValues.length - 1].value); addTransaction(true)">Buy {{ selectedCurrency }}
-            </ion-button>
-            <ion-button
-              @click="calculateValue(currency.historicalValues[currency.historicalValues.length - 2].value); addTransaction(false)">Sell {{ selectedCurrency }}
-            </ion-button>
-          </ion-item>
-          <ion-item>
-            <h3>Total: <strong> {{ result }} </strong> </h3>
-          </ion-item>
-        </ion-list>
-      </ion-card-content>
-    </ion-card>
+    <ion-content fullscreen>
+      <div class="card">
+        <ion-card>
+        <ion-card-header>
+          <ion-card-title>
+            <h1>
+              Buy and sell coins
+            </h1>
+          </ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-list>
+              <ion-item>
+                <ion-select placeholder="Choose a currency" @ionChange="selectedCurrency = $event.detail.value; setCurrency(); selectedCurrencyyy = $event.detail.value">
+                  <ion-select-option :value="symbol" :key="symbol" v-for="symbol in currencyNames">
+                    {{ symbol }}
+                  </ion-select-option>
+                </ion-select>
+              </ion-item>
+              <ion-item>
+                <h2>
+                  Current value:
+                </h2>
+              </ion-item>
+              <ion-item v-if="currency">
+                <h2 class="value">
+                  {{ currency.historicalValues[currency.historicalValues.length - 1].value }}
+                </h2>
+              </ion-item>
+              <ion-item>
+                <ion-input type="number" placeholder="Enter the amount of coins" @ionChange="amntCoinss = $event.detail.value" v-model="amntCoins">
+                </ion-input>
+              </ion-item>
+              <ion-grid fixed>
+                <ion-row>
+                  <ion-col>
+                    <ion-button color="success" class="btn" @click="calculateValue(currency.historicalValues[currency.historicalValues.length - 1].value); addTransaction(true); presentAlert()">
+                      Buy {{ selectedCurrency }}
+                    </ion-button>
+                  </ion-col>
+                  <ion-col>
+                    <ion-button color="danger" class="btn" @click="calculateValue(currency.historicalValues[currency.historicalValues.length - 2].value); addTransaction(false); presentAlert()">
+                      Sell {{ selectedCurrency }}
+                    </ion-button>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+              <ion-item>
+                <h2>
+                  Total value:
+                </h2>
+              </ion-item>
+              <ion-item>
+                <h2 class="value">
+                   {{ result }} 
+                </h2>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+        </ion-card>
+      </div>
+    </ion-content>
   </ion-page>
 </template>
 
@@ -60,6 +78,11 @@ import {
   IonCardContent,
   IonInput,
   IonButton,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  alertController 
 } from "@ionic/vue";
 import { useCurrency } from "@/composables/useCurrency";
 import { useTransaction } from "@/composables/useTransaction";
@@ -78,6 +101,10 @@ export default {
     IonCardContent,
     IonInput,
     IonButton,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol
   },
   setup() {
     const {
@@ -95,6 +122,16 @@ export default {
         amntCoinss
       } = useTransaction();
 
+    const presentAlert = async () => {
+      const alert = await alertController.create({
+        header: 'Success',
+        subHeader: 'Transaction was successful',
+        message: 'Your Transactions has been added to your portfolio',
+        buttons: ['OK']
+      });
+      await alert.present();
+
+    }
     return {
       currencyNames,
       currency,
@@ -105,12 +142,26 @@ export default {
       amntCoins,
       addTransaction,
       selectedCurrencyyy,
-      amntCoinss
+      amntCoinss,
+      presentAlert
     };
   },
 };
 </script>
 
 <style>
+.card {
+  width: 50%;
+ margin: auto;
+ margin-top: 10%;
+}
 
+.value {
+  font-weight: 600;
+}
+
+.btn {
+  width: 100%;
+ margin: auto;
+}
 </style>
